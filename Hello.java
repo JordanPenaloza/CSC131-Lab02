3e0d8670accc266aa54d89f31225d46223383815
JordanPenaloza <jordan332033@gmail.com>
2023-09-25 18:58:05 -0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,20 @@
+package CSC131.Lab02;
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302340727 - This line was added by Jordan Penaloza.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
+}
+
+}
